ee5845150d2e98114d8efb02f5844d5df9b4b3fb
imxande <alerodriguezlarosa@gmail.com>
2020-04-27 06:39:27 -0400

    added timing to check how does the algorithm performs as input increases

--- Big_O.js
+++ Big_O.js
@@ -1,8 +1,11 @@
 // nemo array
 const nemo = ["nemo"];
-
+// creating new array of 1000 items and adding nemo
+const larger = new Array(1000).fill("nemo");
 // find nemo function
 const findNemo = array => {
+    // checking performance
+    let t0 = performance.now();
     // loop through the array
     for (let i = 0; i < array.length; i++){
         // check if array contains nemo
@@ -10,6 +13,9 @@ const findNemo = array => {
             console.log("Found NEMO!");
         }
     }
+    let t1 = performance.now()
+    let time = t1 - t0;
+    console.log(`Call to find nemo took ${time} milliseconds`);
 }
 
-findNemo(nemo);
+findNemo(larger);
